c5ff616ed95a157a59d1218fba0b6ea4b327d906
thilangavinda <thilangavinda@gmail.com>
2023-02-19 12:13:30 +0600

    Add jasypt Encryptor

--- src/main/java/com/thilan/log4j2/Log4j2Application.java
+++ src/main/java/com/thilan/log4j2/Log4j2Application.java
@@ -1,5 +1,6 @@
 package com.thilan.log4j2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/thilan/log4j2/controller/getEndpoints.java
+++ src/main/java/com/thilan/log4j2/controller/getEndpoints.java
@@ -1,6 +1,7 @@
 package com.thilan.log4j2.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class getEndpoints {
 
+    @Value("${jasypt.encryptor.provider-name }")
+    private String dbPw;
+
     @GetMapping("/hello")
     public ResponseEntity<?> getMessage(){
-        log.debug("Inside Controller");
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+        log.debug("Inside Controller:{}",dbPw);
+        return new ResponseEntity<>("Hello World:"+dbPw
+                , HttpStatus.OK);
     }
 
 }
